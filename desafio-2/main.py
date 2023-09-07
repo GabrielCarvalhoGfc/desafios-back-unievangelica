2c8966c687aae9039d56d4964bf239b71817a036
GabrielCarvalhoGfc <143818850+GabrielCarvalhoGfc@users.noreply.github.com>
2023-09-07 17:19:56 -0300

    Update main.py

--- desafio-2/main.py
+++ desafio-2/main.py
@@ -1,12 +1,11 @@
 class Pessoa:
-    def __init__(self, nome,descricao, cpf,programa,animador,imagem):
+    def __init__(self, nome,descricao,programa,animador,imagem):
         self.nome = nome
         self.descricao = descricao
-        self.cpf = cpf
         self.programa = programa
         self.animador = animador
         self.imagem = imagem
 
 if __name__ == "__main__":
-    pessoa1= Pessoa("GABRIEL","MASCULINO","123456,FILME,DISNEY,imagem")
-    print(pessoa1.nome,pessoa1.descricao,pessoa1.cpf,pessoa1.programa,pessoa1.animador,pessoa1.imagem)
+    pessoa1= Pessoa("GABRIEL","MASCULINO","FILME,DISNEY,imagem")
+    print(pessoa1.nome,pessoa1.descricao,pessoa1.programa,pessoa1.animador,pessoa1.imagem)
