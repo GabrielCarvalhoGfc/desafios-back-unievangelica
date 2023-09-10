60ad25eaa1dd43b3eaf6b441a7a403651db7e3aa
GabrielCarvalhoGfc <143818850+GabrielCarvalhoGfc@users.noreply.github.com>
2023-09-10 12:29:09 -0300

    Update desafio3.py

--- desafio3/desafio3.py
+++ desafio3/desafio3.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-pessoas = []
+Characters = []
 
-class Pessoa:
+class Characters:
     def __init__(self, nome,descricao,programa,animador,imagem):
         self.nome = nome
         self.descricao = descricao
@@ -14,23 +14,23 @@ class Pessoa:
 
 
 @app.route("/characters/", methods=['POST'])                                                        
-def criar_pessoa():
-    data = request.get_json()
+def create_Characters():
+    data = crequest.get_json()
     nome = data.get('nome')
     descricao = data.get('descricao')
     programa = data.get('programa')
     animador = data.get('animador')
     imagem = data.get('imagem')
 
-    nova_pessoa = Pessoa(nome,descricao,programa,animador,imagem)
-    pessoas.append(nova_pessoa)    
+    new_characters = Pessoa(nome,descricao,programa,animador,imagem)
+    Characters.append(nova_pessoa)    
 
     return jsonify({'message': 'Pessoa criada com sucesso'}), 201
 
 
 @app.route("/characters/")
 def listar_pessoas():
-    pessoas_data = [{'nome': pessoa.nome, 'descricao': pessoa.descricao,'programa':pessoa.programa,'animador':pessoa.animador,'imagem':pessoa.imagem} for pessoa in pessoas]
+    data_Characters = [{'nome': pessoa.nome, 'descricao': pessoa.descricao,'programa':pessoa.programa,'animador':pessoa.animador,'imagem':pessoa.imagem} for pessoa in pessoas]
     return jsonify(pessoas_data)
 
 
