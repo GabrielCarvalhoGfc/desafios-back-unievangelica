4723e8e965b96487743ec9b130212feadb8c86e1
GabrielCarvalhoGfc <143818850+GabrielCarvalhoGfc@users.noreply.github.com>
2023-09-12 15:52:30 -0300

    Update main.py

--- desafio-2/main.py
+++ desafio-2/main.py
@@ -1,3 +1,4 @@
+# classificar o personagem
 class Characters:
     def __init__(self, nome,descricao,programa,animador,imagem):
         self.nome = nome
@@ -6,6 +7,7 @@ class Characters:
         self.animador = animador
         self.imagem = imagem
 
+#executar a classe
 if __name__ == "__main__":
     Characters1= Characters("GABRIEL","MASCULINO","FILME,DISNEY,imagem")
     print(pessoa1.nome,Characters1.descricao,Characters1.program,Characters1.animador,Characters1.imagem)
